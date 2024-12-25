fac9db75b89f184409a816fd7122b739dba989fb
ReeseHatfield <hatfield.69@wright.edu>
2024-12-25 09:02:38 -0500

    pub script

--- publish.sh
+++ publish.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
+
+echo -e "Attempting to publish project, please ensure the following: \n" \
+    "- All changes have been pushed to git \n" \
+    "- Semver has been updated in Cargo.toml \n" \
+    "- You are sucessfully loged into crates.io via 'cargo login'\n" \
+
+read -p "Do you wish to proceed? [y/n]" -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+
 cargo test
-cargo login
 cargo package
 cargo publish
